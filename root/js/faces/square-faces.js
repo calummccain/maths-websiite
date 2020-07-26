bd0a92e4d7e7243e605526db8adac1e1360b229f
Calum McCain <calummccain@Calums-MacBook-Pro.local>
2020-07-26 12:25:01 +0100

    added three.js, hyperbolic norm and square faces

---
+++ root/js/faces/square-faces.js
@@ -0,0 +1,56 @@
+import * as VF from "../maths-functions/vector-functions.js";
+import * as HF from "../maths-functions/hyperbolic-functions.js"
+
+function hyperboloidFaceBisection(a, b, c, d, n) {
+
+    var j = 0;
+    var coords = [a, b, c, d];
+    var faces = [[0, 1, 2, 3]];
+
+    while (j < n) {
+        var newCoords = [];
+        var newFaces = [];
+        for (var i = 0; i < faces.length; i++) {
+            var u = VF.vectorSum(coords[faces[i][0]], coords[faces[i][1]]);
+            var v = VF.vectorSum(coords[faces[i][1]], coords[faces[i][2]]);
+            var w = VF.vectorSum(coords[faces[i][2]], coords[faces[i][3]]);
+            var x = VF.vectorSum(coords[faces[i][3]], coords[faces[i][0]]);
+            var y = VF.vectorSum(u, w);
+
+            newCoords = newCoords.concat([
+                coords[faces[i][0]],
+                coords[faces[i][1]],
+                coords[faces[i][2]],
+                coords[faces[i][3]],
+                VF.vectorScale(u, 1 / HF.hyperbolicNorm(u)),
+                VF.vectorScale(u, 1 / HF.hyperbolicNorm(v)),
+                VF.vectorScale(u, 1 / HF.hyperbolicNorm(w)),
+                VF.vectorScale(u, 1 / HF.hyperbolicNorm(x)),
+                VF.vectorScale(u, 1 / HF.hyperbolicNorm(y))
+            ]);
+
+            newFaces = newFaces.concat([
+                [9 * i, 9 * i + 4, 9 * i + 8, 9 * i + 7],
+                [9 * i + 1, 9 * i + 5, 9 * i + 8, 9 * i + 4],
+                [9 * i + 2, 9 * i + 6, 9 * i + 8, 9 * i + 5],
+                [9 * i + 3, 9 * i + 7, 9 * i + 8, 9 * i + 6]
+            ]);
+        }
+
+        faces = newFaces;
+        coords = newCoords;
+        j++;
+    }
+
+    var triangularFaces = [];
+
+    for (var i = 0; i < faces.length; i++) {
+        triangularFaces = triangularFaces.concat([
+            [faces[i][0], faces[i][1], faces[i][2]],
+            [faces[i][0], faces[i][2], faces[i][3]]]);
+    }
+
+    return [triangularFaces, coords];
+}
+
+export { hyperboloidFaceBisection };
--- root/js/maths-functions/hyperbolic-functions.js
+++ root/js/maths-functions/hyperbolic-functions.js
@@ -3,32 +3,41 @@ import * as VF from "./vector-functions.js";
 //transforms the original cell to the transformed one 
 // REMEMBER TO MULTIPLY THEM BY f !!!!!!!!!
 function transformVertices(baseVertices, transformation) {
+
     var newVertices = [];
+
     for (var i = 0; i < baseVertices.length; i++) {
         newVertices[i] = VF.transpose(VF.matrixMultiplication(transformation, VF.transpose(baseVertices[i])));
     }
+
     return newVertices;
 }
 
 // turns the word representation of the transformation into the actual matrix representation
 function wordToTransform(word, dictionary) {
+
     var t_matrix = dictionary['e'];
+
     for (var i = 0; i < word.length; i++) {
         var n_matrix = VF.matrixMultiplication(t_matrix, dictionary[word.charAt(i)]);
         t_matrix = n_matrix;
     }
+
     return t_matrix;
 }
 
 // stereographic projection from the hyperboloid model to the poincare model 
 function hyperboloidToPoincare(point) {
+
     var scale = 1 + point[0][0];
     var newPoint = [point[0][1] / scale, point[0][2] / scale, point[0][3] / scale];
+
     return newPoint;
 }
 
 // stereographic projection from the hyperboloid model to the poincare model 
 function hyperboloidToPoincareMod(point) {
+
     if (Math.abs(point[0][0] ** 2 - point[0][1] ** 2 - point[0][2] ** 2 - point[0][3] ** 2) < 0.01) {
         var scale = point[0][0];
         var newPoint = [point[0][1] / scale, point[0][2] / scale, point[0][3] / scale];
@@ -41,25 +50,38 @@ function hyperboloidToPoincareMod(point) {
 
 // stereographic projection from the hyperboloid model to the klein model (use for ideal points too)
 function hyperboloidToKlein(point) {
+
     var scale = point[0][0];
     var newPoint = [point[0][1] / scale, point[0][2] / scale, point[0][3] / scale];
+
     return newPoint;
 }
 
 // 'Cayley transform' from poincare to UHP
 function poincareToUpperHalfPlane(point) {
+
     var x = point[0];
     var y = point[1];
     var z = point[2];
     var s = x ** 2 + y ** 2 + (1 - z) ** 2;
     var uhp = [2 * x / s, 2 * y / s, (1 - x ** 2 - y ** 2) / s];
+
     return uhp;
 }
 
 // hyperboloid inner product
 function hyperboloidInnerProduct(x, y) {
+
     var innerProduct = x[0][0] * y[0][0] - x[0][1] * y[0][1] - x[0][2] * y[0][2] - x[0][3] * y[0][3];
+
     return innerProduct;
 }
 
-export { transformVertices, wordToTransform, hyperboloidInnerProduct, hyperboloidToPoincare, poincareToUpperHalfPlane, hyperboloidToKlein, hyperboloidToPoincareMod };
+function hyperbolicNorm(x) {
+
+    var norm = x[0][0] ** 2 - x[0][1] ** 2 - x[0][2] ** 2 - x[0][3] ** 2;
+
+    return norm;
+}
+
+export { transformVertices, wordToTransform, hyperboloidInnerProduct, hyperboloidToPoincare, poincareToUpperHalfPlane, hyperboloidToKlein, hyperboloidToPoincareMod, hyperbolicNorm };
