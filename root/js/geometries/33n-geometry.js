8c7c7c761d712470e570977cf24b2a5411eea981
Calum McCain <calummccain@Calums-MacBook-Pro.local>
2020-08-10 18:24:43 +0100

    added some more geometries

---
+++ root/js/geometries/33n-geometry.js
@@ -0,0 +1,98 @@
+import * as THREE from "../three.module.js";
+
+import * as ORDER6 from "../data/paracompact/336.js";
+import * as ORDERN from "../data/general-types/33n.js";
+
+import * as FACE from "../faces/klein-square-faces.js";
+
+import * as HF from "../maths-functions/hyperbolic-functions.js";
+import * as VF from "../maths-functions/vector-functions.js";
+
+function hyperbolicTetrahedronGeometry(order, n, transform, s) {
+
+    var vertices = ORDERN.vertices;
+    var faces = ORDERN.faces;
+    var dict, f, center;
+
+    if (order == 6) {
+
+        dict = ORDER6.matrixDict;
+        f = ORDER6.f;
+        center = ORDER6.center;
+
+    } else {
+
+        dict = ORDERN.matrixDict(order);
+        f = ORDERN.f(order);
+        center = ORDERN.center(order);
+
+    }
+
+    var transform = HF.wordToTransform(transform, dict);
+    var newVertices = HF.transformVertices(vertices, transform);
+
+    var kleinVertices = [];
+    for (var i = 0; i < newVertices.length; i++) {
+
+        kleinVertices[i] = [HF.hyperboloidToKlein(VF.transpose(VF.matrixMultiplication(f, VF.transpose(newVertices[i]))))];
+
+    }
+
+    // transform center of cell
+    var newCenter = HF.transformVertices([center], transform);
+    var kleinCenter = [HF.hyperboloidToKlein(VF.transpose(VF.matrixMultiplication(f, VF.transpose(newCenter[0]))))];
+
+    // loop over the faces listed to generate mesh
+    var cellGeometry = new THREE.Geometry();
+
+    for (var i = 0; i < faces.length; i++) {
+
+        var geometry = new THREE.Geometry();
+        var faceData;
+
+        // faceData consists of two arrays - coordinates of mesh vertices in hyperbolic space and which 
+        // vertices correspond to each face
+        //
+        // The coordinates are originally in the form of a row vector so transposes are required as well 
+        // as multiplication by the f matrix to get them into the standard coordinates
+
+        faceData = FACE.kleinFace(
+            kleinVertices[faces[i][0]],
+            kleinVertices[faces[i][1]],
+            kleinVertices[faces[i][2]],
+            n
+        );
+
+        // facets is the list of small trianglular faces that make up the mesh and which vertices make them up 
+        var facets = faceData[0];
+        var hyperboloidVertices = faceData[1];
+
+        // transform the vertices to the appropriate model - poincare disk or hyperboloid model
+        // scale them to the center
+        // add them to the geometry
+        for (var j = 0; j < hyperboloidVertices.length; j++) {
+
+            var vertex = HF.kleinToPoincare(hyperboloidVertices[j]);
+            var vertex2 = VF.vectorSum(VF.vectorScale(vertex, 1 - s), VF.vectorScale(kleinCenter, s));
+            geometry.vertices.push(new THREE.Vector3(vertex2[0][0], vertex2[0][1], vertex2[0][2]));
+
+        }
+
+        // add the facets to the geometry
+
+        for (var k = 0; k < facets.length; k++) {
+
+            var facetPiece = facets[k];
+            geometry.faces.push(new THREE.Face3(facetPiece[(transform.length) % 2], facetPiece[2], facetPiece[(transform.length + 1) % 2]));
+
+        }
+
+        cellGeometry.merge(geometry);
+        cellGeometry.mergeVertices();
+
+    }
+
+    return cellGeometry;
+}
+
+export { hyperbolicTetrahedronGeometry };
--- root/js/main/klein-main-2.js
+++ root/js/main/klein-main-2.js
@@ -1,5 +1,6 @@
 import * as THREE from "../three.module.js";
-import * as GEOM from "../geometries/53n-geometry.js"
+import * as GEOM53N from "../geometries/53n-geometry.js"
+import * as GEOM43N from "../geometries/43n-geometry.js"
 
 var scene, camera, renderer;
 var objects = [];
@@ -7,7 +8,7 @@ var objects = [];
 var WIDTH = window.innerWidth;
 var HEIGHT = window.innerHeight;
 
-function init(n, transforms, opacityValue, s, order) {
+function init(n, transforms, opacityValue, s, schlafli) {
 
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xf2f2f2);
@@ -18,7 +19,7 @@ function init(n, transforms, opacityValue, s, order) {
     light.position.set(1, -1, 1);
     scene.add(light);
 
-    initObjects(n, transforms, opacityValue, s, order);
+    initObjects(n, transforms, opacityValue, s, schlafli);
 
     initCamera();
     initRenderer();
@@ -37,7 +38,7 @@ function initRenderer() {
     renderer.setSize(WIDTH, HEIGHT);
 }
 
-function initObjects(n, transforms, opacityValue, s, order) {
+function initObjects(n, transforms, opacityValue, s, schlafli) {
 
     // loop over the each transformation in transformation array
 
@@ -57,7 +58,18 @@ function initObjects(n, transforms, opacityValue, s, order) {
             transparent: true
         });
 
-        var geometry = GEOM.hyperbolicDodecahedronGeometry(order, n, transforms[m], s);
+        var geometry;
+        var type = schlafli.slice(0, 2);
+
+        if (type === '53') {
+
+            geometry = GEOM53N.hyperbolicDodecahedronGeometry(schlafli[2], n, transforms[m], s);
+
+        } else if (type === '43') {
+
+            geometry = GEOM43N.hyperbolicCubeGeometry(schlafli[2], n, transforms[m], s);
+
+        }
 
         var cell = new THREE.Mesh(geometry, material);
 
@@ -76,6 +88,8 @@ function rotateObjects() {
         objects[i].rotation.x -= 0;
         objects[i].rotation.y -= 0.005;
         objects[i].rotation.z -= 0.01;
+
+        //objects[i].position.set(i, 0, 0);
 
     }
 }
