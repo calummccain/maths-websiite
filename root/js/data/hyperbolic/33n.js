47e9e7eb7eacbe4956e795e784af06388863cc75
Calum McCain <calummccain@Calums-MacBook-Pro.local>
2020-09-13 00:26:26 +0100

    33n updated

--- root/js/data/hyperbolic/33n.js
+++ root/js/data/hyperbolic/33n.js
@@ -1,18 +1,4 @@
-// Order n cubic
-
-function typeOfHoneycomb(n) {
-    var t = '';
-    if (n < 6) {
-        t = 'compact';
-    } else if (n == 6) {
-        t = 'paracompact';
-    } else {
-        t = 'noncompact';
-    }
-    return t;
-}
-
-const faceType = 'triangle';
+// Order n tetrahedral
 
 const vertices = [
     [1, 1, 1, 1],
@@ -29,101 +15,141 @@ const faces = [
 ];
 
 //cfv
-const a = [
-    [1, 0, 0, 0],
-    [0, 0, 1, 0],
-    [0, 1, 0, 0],
-    [0, 0, 0, 1]
-];
+// const a = [
+//     [1, 0, 0, 0],
+//     [0, 0, 1, 0],
+//     [0, 1, 0, 0],
+//     [0, 0, 0, 1]
+// ];
+
+function a(v) {
+    return [v[0], v[2], v[1], v[3]];
+}
 
 //cev
-const b = [
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 0, 1],
-    [0, 0, 1, 0]
-];
+// const b = [
+//     [1, 0, 0, 0],
+//     [0, 1, 0, 0],
+//     [0, 0, 0, 1],
+//     [0, 0, 1, 0]
+// ];
+
+function b(v) {
+    return [v[0], v[1], v[3], v[2]];
+}
 
 //cfe
-const c = [
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 0, -1],
-    [0, 0, -1, 0]
-];
+// const c = [
+//     [1, 0, 0, 0],
+//     [0, 1, 0, 0],
+//     [0, 0, 0, -1],
+//     [0, 0, -1, 0]
+// ];
+
+function c(v) {
+    return [v[0], v[1], -v[3], -v[2]];
+}
 
 //fev
-function d(n) {
+function d(n, v) {
 
     var cos = Math.cos(Math.PI / n) ** 2;
     var sin = Math.sin(Math.PI / n) ** 2;
 
-    var matrix = [
-        [2 - 3 * sin, 3 * sin - 1, 3 * sin - 1, 1 - 3 * sin],
-        [cos, sin, -cos, cos],
-        [cos, -cos, sin, cos],
-        [-cos, cos, cos, sin]
+    var newVector = [
+        (2 - 3 * sin) * v[0] + (3 * sin - 1) * v[1] + (3 * sin - 1) * v[2] + (1 - 3 * sin) * v[3],
+        cos * v[0] + sin * v[1] - cos * v[2] + cos * v[3],
+        cos * v[0] - cos * v[1] + sin * v[2] + cos * v[3],
+        -cos * v[0] + cos * v[1] + cos * v[2] + sin * v[3]
     ];
-    return matrix;
+    return newVector;
 
 }
 
-const e = [
-    [1, 0, 0, 0],
-    [0, 1, 0, 0],
-    [0, 0, 1, 0],
-    [0, 0, 0, 1]
-];
+// var cos = Math.cos(Math.PI / n) ** 2;
+//     var sin = Math.sin(Math.PI / n) ** 2;
 
-function f(n) {
+//     var matrix = [
+//         [2 - 3 * sin, 3 * sin - 1, 3 * sin - 1, 1 - 3 * sin],
+//         [cos, sin, -cos, cos],
+//         [cos, -cos, sin, cos],
+//         [-cos, cos, cos, sin]
+//     ];
+//     return matrix;
 
-    var matrix;
+// const e = [
+//     [1, 0, 0, 0],
+//     [0, 1, 0, 0],
+//     [0, 0, 1, 0],
+//     [0, 0, 0, 1]
+// ];
+
+function e(v) {
+    return [v[0], v[1], v[2], v[3]];
+}
+
+
+function f(n, v) {
+
+    var newVector;
     var cot = 1 / (Math.tan(Math.PI / n) ** 2);
 
-    if (n < 6) {
+    if (n == 6) {
 
-        matrix = [
-            [Math.sqrt(cot / (2 * (3 - cot))), 0, 0, 0],
-            [0, Math.sqrt((cot - 2) / (2 * (3 - cot))), 0, 0],
-            [0, 0, Math.sqrt((cot - 2) / (2 * (3 - cot))), 0],
-            [0, 0, 0, Math.sqrt((cot - 2) / (2 * (3 - cot)))]
-        ];
-
-    } else if (n == 6) {
-
-        matrix = [
-            [Math.sqrt(3), 0, 0, 0],
-            [0, 1, 0, 0],
-            [0, 0, 1, 0],
-            [0, 0, 0, 1]
-        ]
+        newVector = [Math.sqrt(3) * v[0], v[1], v[2], v[3]];
 
     } else {
 
-        matrix = [
-            [Math.sqrt(Math.abs(cot / (2 * (3 - cot)))), 0, 0, 0],
-            [0, Math.sqrt(Math.abs((cot - 2) / (2 * (3 - cot)))), 0, 0],
-            [0, 0, Math.sqrt(Math.abs((cot - 2) / (2 * (3 - cot)))), 0],
-            [0, 0, 0, Math.sqrt(Math.abs((cot - 2) / (2 * (3 - cot))))]
+        newVector = [
+            Math.sqrt(Math.abs(cot / (2 * (3 - cot)))) * v[0],
+            Math.sqrt(Math.abs((cot - 2) / (2 * (3 - cot)))) * v[1],
+            Math.sqrt(Math.abs((cot - 2) / (2 * (3 - cot)))) * v[2],
+            Math.sqrt(Math.abs((cot - 2) / (2 * (3 - cot)))) * v[3]
         ];
 
     }
-    return matrix;
+
+    return newVector;
 }
 
-function matrixDict(n) {
+// matrix = [
+//     [Math.sqrt(3), 0, 0, 0],
+//     [0, 1, 0, 0],
+//     [0, 0, 1, 0],
+//     [0, 0, 0, 1]
+// ]
 
-    var dict = {
-        'a': a,
-        'b': b,
-        'c': c,
-        'd': d(n),
-        'e': e,
-        'f': f(n)
+// newVector = [
+//     [Math.sqrt(Math.abs(cot / (2 * (3 - cot)))), 0, 0, 0],
+//     [0, Math.sqrt(Math.abs((cot - 2) / (2 * (3 - cot)))), 0, 0],
+//     [0, 0, Math.sqrt(Math.abs((cot - 2) / (2 * (3 - cot)))), 0],
+//     [0, 0, 0, Math.sqrt(Math.abs((cot - 2) / (2 * (3 - cot))))]
+// ];
+
+function matrixDict(n, letter, vector) {
+    var newVector;
+    switch (letter) {
+        case 'a':
+            newVector = a(vector);
+            break;
+        case 'b':
+            newVector = b(vector);
+            break;
+        case 'c':
+            newVector = c(vector);
+            break;
+        case 'd':
+            newVector = d(n, vector);
+            break;
+        case 'e':
+            newVector = e(vector);
+            break;
+        case 'f':
+            newVector = f(n, vector);
+            break;
     }
-
-    return dict;
-}
+    return newVector;
+};
 
 const faceReflections = ['', 'cab', 'ab', 'b'];
 
@@ -134,4 +160,4 @@ function center(n) {
     return [1 / Math.sqrt(Math.abs(cot / (2 * (3 - cot)))), 0, 0, 0]
 }
 
-export { typeOfHoneycomb, faceType, vertices, faces, a, b, c, d, e, f, matrixDict, faceReflections, center };
+export { vertices, faces, a, b, c, d, e, f, matrixDict, faceReflections, center };
