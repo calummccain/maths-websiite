ba0523bf7af7a9d039d7cf78c92b832f9e81a79d
Calum McCain <calummccain@Calums-MacBook-Pro.local>
2020-11-02 21:48:43 +0000

    tidy up

--- root/js/geometries/120-cell-geometry.js
+++ root/js/geometries/120-cell-geometry.js
@@ -2,11 +2,9 @@ import * as CXX from "../data/polychorons/120-cell.js";
 import * as GEOM from "./spherical-geometry.js";
 import * as LOAD from "../load-data.js";
 
-console.log("hello calum");
-console.log(LOAD.loadJSON("/root/js/data/polychorons/json-data/120-data.json"));
-console.log("no problems?");
-
 function cxxCellGeometry(refinement, cellName, d) {
+
+    var data = LOAD.loadJSON("/root/js/data/polychorons/json-data/120-data.json");
 
     var cellFaceDict = CXX.cellFaceDict;
     var vertexDict = CXX.vertexDict;
