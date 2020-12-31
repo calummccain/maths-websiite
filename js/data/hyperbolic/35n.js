59da36bdfd1e425c48ab4113791856b3077fb343
Calum McCain <cmccain97@gmail.com>
2020-12-31 19:17:55 +0000

    line+gitignore

--- js/data/hyperbolic/35n.js
+++ js/data/hyperbolic/35n.js
@@ -42,6 +42,39 @@ const faces = [
     [7, 11, 9]
 ];
 
+const lines = [
+    [0, 2],
+    [0, 4],
+    [0, 6],
+    [0, 8],
+    [0, 9],
+    [1, 3],
+    [1, 4],
+    [1, 6],
+    [1, 10], 
+    [1, 11],
+    [2, 5],
+    [2, 7],
+    [2, 8],
+    [2, 9],
+    [3, 5],
+    [3, 7],
+    [3, 10],
+    [3, 11],
+    [4, 6],
+    [4, 8],
+    [4, 10],
+    [5, 7],
+    [5, 8],
+    [5, 10],
+    [6, 9],
+    [6, 11],
+    [7, 9],
+    [7, 11],
+    [8, 10],
+    [9, 11]
+]
+
 function a(v) {
 
     return [v[0], v[1], v[2], -v[3]];
@@ -183,4 +216,4 @@ function center(n) {
 
 }
 
-export { vertices, faces, a, b, c, d, e, f, matrixDict, faceReflections, center };
+export { vertices, faces, a, b, c, d, e, f, matrixDict, faceReflections, center, lines };
