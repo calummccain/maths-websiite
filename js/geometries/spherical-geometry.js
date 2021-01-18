4a1fabdaf1108cb81d375b1528f27a180909d769
Calum McCain <cmccain97@gmail.com>
2021-01-18 12:33:58 +0000

    533 and 343 now working

--- js/geometries/spherical-geometry.js
+++ js/geometries/spherical-geometry.js
@@ -3,59 +3,6 @@ import * as FACE from "../faces/spherical-faces.js";
 import * as SF from "../maths-functions/spherical-functions.js";
 import * as VF from "../maths-functions/vector-functions.js";
 
-// function sphericalGeometry(cellName, cellFaceDict, vertexDict, refinement, d, sideNumber, faceDict) {
-
-//     var faces = cellFaceDict[cellName];
-
-//     var cell = [];
-
-//     for (var i = 0; i < faces.length; i++) {
-
-//         var geometry = new THREE.Geometry();
-//         var faceVertices = [];
-
-//         for (var j = 0; j < sideNumber; j++) {
-
-//             if (sideNumber == 5) {
-
-//                 faceVertices.push(vertexDict[faceDict[faces[i]][j]]);
-
-//             } else {
-
-//                 faceVertices.push(vertexDict[faces[i][j]]);
-
-//             }
-
-//         }
-
-//         var faceData = FACE.sphereFace(faceVertices, refinement);
-
-//         var facets = faceData[0];
-//         var hypersphereVertices = faceData[1];
-
-//         for (var j = 0; j < hypersphereVertices.length; j++) {
-
-//             var vertex = VF.vectorScale(hypersphereVertices[j], 1 / Math.sqrt(SF.sphereNorm(hypersphereVertices[j])));
-//             var vertex2 = SF.sphereToPoincare(vertex, d);
-//             geometry.vertices.push(new THREE.Vector3(vertex2[0], vertex2[1], vertex2[2]));
-
-//         }
-
-//         for (var k = 0; k < facets.length; k++) {
-
-//             var facetPiece = facets[k];
-//             geometry.faces.push(new THREE.Face3(facetPiece[0], facetPiece[1], facetPiece[2]));
-
-//         }
-
-//         geometry.mergeVertices();
-//         geometry.name = [faces[i], cellName];
-//         cell.push(geometry);
-
-//     }
-
-//     return cell;
-// }
 
 function sphericalGeometry(vertices, faces, matrixDict, transform, numberOfSides, refinement, names, d) {
 
@@ -76,8 +23,6 @@ function sphericalGeometry(vertices, faces, matrixDict, transform, numberOfSides
 
     }
 
-    console.log(properVertices)
-
     var cellGeometry = [];
 
     for (var i = 0; i < faces.length; i++) {
@@ -87,8 +32,6 @@ function sphericalGeometry(vertices, faces, matrixDict, transform, numberOfSides
         var faceVertices = Array(numberOfSides).fill().map(() => initial++);
         faceVertices = faceVertices.map((x) => properVertices[faces[i][x]]);
         var faceData = FACE.sphereFace(faceVertices, refinement);
-
-        console.log(faceData)
 
         var facets = faceData[0];
         var hypersphereVertices = faceData[1];
@@ -122,5 +65,6 @@ function sphericalGeometry(vertices, faces, matrixDict, transform, numberOfSides
     return cellGeometry;
 
 }
+
 
 export { sphericalGeometry };
