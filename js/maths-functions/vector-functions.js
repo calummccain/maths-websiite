85bd4f4beb310f32d343e3fd6b25dc30a80c6fca
Calum McCain <cmccain97@gmail.com>
2021-01-27 19:28:49 +0000

    vector functions tidied

--- js/maths-functions/vector-functions.js
+++ js/maths-functions/vector-functions.js
@@ -5,7 +5,7 @@ function vectorSum(x, y) {
 
     for (var i = 0; i < x.length; i++) {
 
-        vSum[i] = x[i] + y[i];
+        vSum.push(x[i] + y[i]);
 
     }
 
@@ -19,7 +19,7 @@ function vectorDiff(x, y) {
 
     for (var i = 0; i < x.length; i++) {
 
-        vDiff[i] = x[i] - y[i];
+        vDiff.push(x[i] - y[i]);
 
     }
 
@@ -34,7 +34,7 @@ function vectorScale(x, s) {
 
     for (var i = 0; i < x.length; i++) {
 
-        vScale[i] = x[i] * s;
+        vScale.push(x[i] * s);
 
     }
 
@@ -42,18 +42,18 @@ function vectorScale(x, s) {
 
 }
 
+// returns 3D cross product of two vectors
 function vectorCross(a, b) {
 
-    var norm = [
+    return norm = [
         a[1] * b[2] - a[2] * b[1],
         a[2] * b[0] - a[0] * b[2],
         a[0] * b[1] - a[1] * b[0]
     ];
 
-    return norm;
-
 }
 
+// returns nD dot product
 function vectorDot(a, b) {
 
     var dot = 0;
@@ -68,95 +68,114 @@ function vectorDot(a, b) {
 
 }
 
+// calculates intersection of a line and a sphere 
+// TODO: assumes line intersects sphere
 function lineSphereIntersection(a, b) {
 
     var ab = vectorDiff(a, b);
 
-    var A = vectorDot(ab, ab);
-    var B = 2 * vectorDot(b, ab);
-    var C = vectorDot(b, b) - 1;
+    var A = vectorDot(ab, ab), B = 2 * vectorDot(b, ab), C = vectorDot(b, b) - 1;
 
     var l1 = (-B + Math.sqrt(B ** 2 - 4 * A * C)) / (2 * A);
     var l2 = (-B - Math.sqrt(B ** 2 - 4 * A * C)) / (2 * A);
 
     var l = Math.min(l1, l2);
-    var intersection = vectorSum(vectorScale(a, l), vectorScale(b, 1 - l));
-
-    return intersection;
+    return intersection = vectorSum(vectorScale(a, l), vectorScale(b, 1 - l));
 
 }
 
 // midpoint of a line
 function midpoint(a, b, c, compact) {
 
-    var newVector;
-
-    // if ((norm(a) > 0.99999) && (norm(b) > 0.9999) && (compact === "uncompact")) {
-
-    //     //var halfDist = Math.sqrt((vectorDot(a, b) + 1) / 2);
-    //     var shiftedA = vectorDiff(a, c);
-    //     var shiftedB = vectorDiff(b, c);
-    //     var rad = norm(shiftedA);
-    //     //console.log(c, norm(c), rad, norm(shiftedB));
-    //     var unnormalisedSum = vectorScale(vectorSum(shiftedA, shiftedB), 1 / 2);
-    //     newVector = vectorSum(c, vectorScale(unnormalisedSum, rad / norm(unnormalisedSum)));
-
-    // } else {
-
-    //     newVector = vectorScale(vectorSum(a, b), 1 / 2);
-
-    // }
-
-    newVector = vectorScale(vectorSum(a, b), 0.5);
-
-    return newVector;
+    return vectorScale(vectorSum(a, b), 0.5);
 
 }
 
+// returns nD euclidean norm of a vector
 function norm(x) {
 
-    return Math.sqrt((x[0] ** 2) + (x[1] ** 2) + (x[2] ** 2));
+    return Math.sqrt(vectorDot(x, x));
 
 }
 
-// function lineSphereIntersection(a, b) {
+// returns distance between two points
+function distance(x, y) {
 
-//     var a_b = vectorDiff(a, b);
+    return norm(vectorDiff(x, y));
 
-//     var delta = Math.sqrt(vectorDot(b, a_b) ** 2 - (norm(a_b) ** 2) * (norm(b) ** 2 - 1));
+}
 
-//     var t1 = (-vectorDot(b, a_b) + delta) / norm(a_b) ** 2;
-//     var t2 = (-vectorDot(b, a_b) - delta) / norm(a_b) ** 2;
-
-//     var x1 = vectorSum(vectorScale(a, t1), vectorScale(b, 1 - t1));
-//     var x2 = vectorSum(vectorScale(a, t2), vectorScale(b, 1 - t2));
-
-//     return [x1, x2];
-
-// }
-
+// returns circumradius of a triangle with sides of length a, b, c
 function circumradius(a, b, c) {
 
     return (a * b * c) / Math.sqrt(Math.abs((a + b + c) * (b + c - a) * (c + a - b) * (a + b - c)));
 
 }
 
-function determinant(m) {
+// returns circumcenter of a triangle with vertces u, v, w
+function circumcenter(u, v, w) {
+
+    const a = determinant([[u[0], u[1], 1], [v[0], v[1], 1], [w[0], w[1], 1]]);
+    const ru = u[0] ** 2 + u[1] ** 2, rv = v[0] ** 2 + v[1] ** 2, rw = w[0] ** 2 + w[1] ** 2;
+    const bx = -determinant([[ru, u[1], 1], [rv, v[1], 1], [rw, w[1], 1]]);
+    const by = determinant([[ru, u[0], 1], [rv, v[0], 1], [rw, w[0], 1]]);
+
+    return [-bx / (2 * a), -by / (2 * a)]
+
+}
+
+// returns 2x2 determinant
+function determinant2(m) {
+
+    return m[0][0] * m[1][1] - m[0][1] * m[1][0];
+
+}
+
+// returns 3x3 determinat
+function determinant3(m) {
 
     return m[0][0] * (m[1][1] * m[2][2] - m[1][2] * m[2][1]) - m[0][1] * (m[1][0] * m[2][2] - m[1][2] * m[2][0]) + m[0][2] * (m[1][0] * m[2][1] - m[1][1] * m[2][0]);
 
 }
 
-function circumcenter(u, v, w) {
+// transforms the baseVertices with the word 'transformation' using the dictionary
+function transformVertices(baseVertices, transformation, dictionary) {
 
-    const a = determinant([[u[0], u[1], 1], [v[0], v[1], 1], [w[0], w[1], 1]]);
-    const ru = u[0] ** 2 + u[1] ** 2;
-    const rv = v[0] ** 2 + v[1] ** 2;
-    const rw = w[0] ** 2 + w[1] ** 2;
-    const bx = -determinant([[ru, u[1], 1], [rv, v[1], 1], [rw, w[1], 1]]);
-    const by = determinant([[ru, u[0], 1], [rv, v[0], 1], [rw, w[0], 1]]);
+    var newVertices = [];
+    var e1 = [1, 0, 0, 0], e2 = [0, 1, 0, 0], e3 = [0, 0, 1, 0], e4 = [0, 0, 0, 1];
 
-    return [-bx / (2 * a), -by / (2 * a)]
+    if (transformation !== '') {
+
+        for (var i = transformation.length - 1; i > -1; i--) {
+
+            e1 = dictionary(transformation[i], e1);
+            e2 = dictionary(transformation[i], e2);
+            e3 = dictionary(transformation[i], e3);
+            e4 = dictionary(transformation[i], e4);
+
+        }
+
+    }
+
+    for (var j = 0; j < baseVertices.length; j++) {
+
+        var oldVertex = baseVertices[j];
+        var newVertex = vectorSum(
+            vectorSum(
+                vectorScale(e1, oldVertex[0]),
+                vectorScale(e2, oldVertex[1])
+            ),
+            vectorSum(
+                vectorScale(e3, oldVertex[2]),
+                vectorScale(e4, oldVertex[3])
+            )
+        );
+
+        newVertices.push(newVertex);
+
+    }
+
+    return newVertices;
 
 }
 
@@ -168,8 +187,11 @@ export {
     vectorDot,
     midpoint,
     norm,
+    distance,
     lineSphereIntersection,
     circumradius,
     circumcenter,
-    determinant
+    determinant2,
+    determinant3,
+    transformVertices
 }
