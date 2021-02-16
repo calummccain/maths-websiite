6b9dd36c2008895ecfbbfc5b0e7e87c8b4cabb8a
Calum McCain <cmccain97@gmail.com>
2021-02-16 00:06:50 +0000

    35n in progress

--- js/data/35n.js
+++ js/data/35n.js
@@ -1,80 +1,36 @@
 // order n icosahedral
 
 import { p } from "./constants.js";
+import { boundaries } from "./geometry-decider.js";
 
 const icosahedronData = (n) => {
 
     return {
-        
+
         vertices: [
-            [1, 1, p, 0],
-            [1, 1, -p, 0],
-            [1, -1, p, 0],
-            [1, -1, -p, 0],
-            [1, p, 0, 1],
-            [1, -p, 0, 1],
-            [1, p, 0, -1],
-            [1, -p, 0, -1],
-            [1, 0, 1, p],
-            [1, 0, 1, -p],
-            [1, 0, -1, p],
-            [1, 0, -1, -p]
+            [1, 1, p, 0], [1, 1, -p, 0], [1, -1, p, 0],
+            [1, -1, -p, 0], [1, p, 0, 1], [1, -p, 0, 1],
+            [1, p, 0, -1], [1, -p, 0, -1], [1, 0, 1, p],
+            [1, 0, 1, -p], [1, 0, -1, p], [1, 0, -1, -p]
         ],
 
         edges: [
-            [0, 2],
-            [0, 4],
-            [0, 6],
-            [0, 8],
-            [0, 9],
-            [1, 3],
-            [1, 4],
-            [1, 6],
-            [1, 10],
-            [1, 11],
-            [2, 5],
-            [2, 7],
-            [2, 8],
-            [2, 9],
-            [3, 5],
-            [3, 7],
-            [3, 10],
-            [3, 11],
-            [4, 6],
-            [4, 8],
-            [4, 10],
-            [5, 7],
-            [5, 8],
-            [5, 10],
-            [6, 9],
-            [6, 11],
-            [7, 9],
-            [7, 11],
-            [8, 10],
-            [9, 11]
+            [0, 2], [0, 4], [0, 6], [0, 8],
+            [0, 9], [1, 3], [1, 4], [1, 6],
+            [1, 10], [1, 11], [2, 5], [2, 7],
+            [2, 8], [2, 9], [3, 5], [3, 7],
+            [3, 10], [3, 11], [4, 6], [4, 8],
+            [4, 10], [5, 7], [5, 8], [5, 10],
+            [6, 9], [6, 11], [7, 9], [7, 11],
+            [8, 10], [9, 11]
         ],
 
         faces: [
-            [0, 8, 2],
-            [0, 2, 9],
-            [0, 6, 4],
-            [0, 4, 8],
-            [0, 9, 6],
-            [1, 3, 10],
-            [1, 11, 3],
-            [1, 4, 6],
-            [1, 10, 4],
-            [1, 6, 11],
-            [2, 5, 7],
-            [2, 8, 5],
-            [2, 7, 9],
-            [3, 7, 5],
-            [3, 5, 10],
-            [3, 11, 7],
-            [4, 10, 8],
-            [5, 8, 10],
-            [6, 9, 11],
-            [7, 11, 9]
+            [0, 8, 2], [0, 2, 9], [0, 6, 4], [0, 4, 8],
+            [0, 9, 6], [1, 3, 10], [1, 11, 3], [1, 4, 6],
+            [1, 10, 4], [1, 6, 11], [2, 5, 7], [2, 8, 5],
+            [2, 7, 9], [3, 7, 5], [3, 5, 10], [3, 11, 7],
+            [4, 10, 8], [5, 8, 10], [6, 9, 11], [7, 11, 9]
         ],
 
         numVertices: 12,
@@ -85,24 +41,32 @@ const icosahedronData = (n) => {
 
         numSides: 3,
 
+        // CEV
+        // (0, 0, 0, 1)
         a: (v) => {
 
             return [v[0], v[1], v[2], -v[3]];
 
         },
 
+        // CFE
+        // (0, 1, 0, 0)
         b: (v) => {
 
             return [v[0], -v[1], v[2], v[3]];
 
         },
 
+        // CFV
+        // (0, p, -1, 1 / p)
         c: (v) => {
 
             return [v[0], (v[1] + v[2] / p - p * v[3]) / 2, (v[1] / p + p * v[2] + v[3]) / 2, (-p * v[1] + v[2] - v[3] / p) / 2];
 
         },
 
+        // FEV
+        // (p cot ** 2 - 1 / p ** 3, 0, p ** 2, 1)
         d: (v) => {
 
             if (n == 3) {
@@ -162,26 +126,11 @@ const icosahedronData = (n) => {
         },
 
         faceReflections: [
-            '',
-            'a',
-            'caca',
-            'ca',
-            'aca',
-            'cbacabcbaca',
-            'acbacabcbaca',
-            'cbabcbaca',
-            'cabcbaca',
-            'acabcbaca',
-            'bcaca',
-            'bca',
-            'baca',
-            'bcbabcbaca',
-            'bcabcbaca',
-            'bacabcbaca',
-            'cbaca',
-            'bcbaca',
-            'babcbaca',
-            'abcbaca'
+            '', 'a', 'caca', 'ca', 'aca', 'cbacabcbaca',
+            'acbacabcbaca', 'cbabcbaca', 'cabcbaca',
+            'acabcbaca', 'bcaca', 'bca', 'baca',
+            'bcbabcbaca', 'bcabcbaca', 'bacabcbaca',
+            'cbaca', 'bcbaca', 'babcbaca', 'abcbaca'
         ],
 
         outerReflection: "d",
@@ -201,57 +150,11 @@ const icosahedronData = (n) => {
 
         },
 
-        // TODO what goes in the else columnn?
+        // 3 4 5 6 7
+        // h u u u u
         metric: () => {
 
-            if (n == 3) {
-
-                return "hyperbolic";
-
-            } else if (n == 4) {
-
-                return "hyperbolic";
-
-            } else if (n == 5) {
-
-                return "hyperbolic";
-
-            } else if (n == 6) {
-
-                return "hyperbolic";
-
-            } else {
-
-                return "hyperbolic";
-
-            }
-
-        },
-
-        // TODO what goes in the else columnn?
-        compact: () => {
-
-            if (n == 3) {
-
-                return "compact";
-
-            } else if (n == 4) {
-
-                return "uncompact";
-
-            } else if (n == 5) {
-
-                return "uncompact";
-
-            } else if (n == 6) {
-
-                return "uncompact";
-
-            } else {
-
-                return "uncompact";
-
-            }
+            return boundaries(n, Math.PI / Math.atan(p ** 2), 10 / 3);
 
         },
 
