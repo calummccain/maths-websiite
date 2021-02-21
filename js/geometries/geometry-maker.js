0361daadd7b6720f015662fd39f8fc549d56966b
Calum McCain <cmccain97@gmail.com>
2021-02-21 18:23:00 +0000

    geometry maker

---
+++ js/geometries/geometry-maker.js
@@ -0,0 +1,39 @@
+import { sphericalGeometry } from "./spherical-geometry.js";
+import { euclideanGeometry } from "./euclidean-geometry.js";
+import { hyperbolicGeometry } from "./hyperbolic-geometry.js";
+
+function honeycombGeometry(data, transform, refinement, model) {
+
+    const d = 1;
+
+    var honeycomb;
+
+    if (data.metric == "s") {
+
+        refinement += 1;
+        honeycomb = sphericalGeometry(data, transform, refinement, d);
+
+    } else if (data.metric == "e") {
+
+        honeycomb = euclideanGeometry(data, transform);
+
+    } else if (data.metric == "h" || data.metric == "p" || data.metric == "u") {
+
+        if (data.cellType === "euclidean" || data.cellType === "hyperbolic") {
+
+            const newVertices = [];
+            data.vertices.forEach((v) => { newVertices.push(data.flip(v)) });
+            data.vertices = newVertices;
+
+        }
+
+        honeycomb = hyperbolicGeometry(data, transform, refinement, model);
+
+    }
+
+
+    return honeycomb;
+
+}
+
+export { honeycombGeometry };
