ed8c43227132bdb3eb8c12d1dc273792bdaeff56
Calum McCain <cmccain97@gmail.com>
2021-04-08 22:25:22 +0100

    updated euclidean face

--- js/geometries/euclidean-geometry.js
+++ js/geometries/euclidean-geometry.js
@@ -1,50 +1,18 @@
 import * as THREE from "../three-bits/three.module.js";
 import { euclideanFace } from "../faces/euclidean-faces.js";
 import * as VF from "../maths-functions/vector-functions.js";
-
+import { matrixDict } from "../data/matrix-dictionary.js";
 
 function euclideanGeometry(data, transform) {
 
     // matrix dictionary
-    function matrixDict(letter, vector) {
-    
-        if (letter === "a") {
-    
-            return data.a(vector)
-    
-        } else if (letter === "b") {
-
-            return data.b(vector)
-
-        } else if (letter === "c") {
-
-            return data.c(vector)
-
-        } else if (letter === "d") {
-
-            return data.d(vector)
-
-        } else if (letter === "e") {
-
-            return data.e(vector)
-
-        } else if (letter === "f") {
-
-            return data.f(vector)
-
-        } else {
-
-            throw 'letter needs to be one of a, b, c, d, e, f!';
-
-        }
-
-    }
+    var matrix = matrixDict(data);
 
     // Transform the 'central' cell's vertices to the transformed cell's vertices
-    var newVertices = VF.transformVertices(data.vertices, transform, matrixDict);
-    
+    var newVertices = VF.transformVertices(data.vertices, transform, matrix);
+
     var properVertices = [];
-    
+
     for (var i = 0; i < newVertices.length; i++) {
 
         properVertices[i] = data.f(newVertices[i]);
@@ -52,17 +20,18 @@ function euclideanGeometry(data, transform) {
     }
 
     var cellGeometry = [];
+    var faceGeometry, initial, faceVertices, faceData, facets, euclideanVertices, facetPiece;
 
     for (var i = 0; i < data.numFaces; i++) {
 
-        var faceGeometry = new THREE.Geometry();
-        var initial = 0;
-        var faceVertices = Array(data.numSides).fill().map(() => initial++);
+        faceGeometry = new THREE.Geometry();
+        initial = 0;
+        faceVertices = Array(data.numSides).fill().map(() => initial++);
         faceVertices = faceVertices.map((x) => properVertices[data.faces[i][x]]);
-        var faceData = euclideanFace(faceVertices);
+        faceData = euclideanFace(faceVertices);
 
-        var facets = faceData[0];
-        var euclideanVertices = faceData[1];
+        facets = faceData[0];
+        euclideanVertices = faceData[1];
 
         for (var j = 0; j < euclideanVertices.length; j++) {
 
@@ -72,7 +41,7 @@ function euclideanGeometry(data, transform) {
 
         for (var k = 0; k < facets.length; k++) {
 
-            var facetPiece = facets[k];
+            facetPiece = facets[k];
             faceGeometry.faces.push(new THREE.Face3(facetPiece[0], facetPiece[1], facetPiece[2]));
 
         }
