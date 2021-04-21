cb7b1affc729e7a2709ab9d0260b530474b1d273
Calum McCain <cmccain97@gmail.com>
2021-04-21 22:13:54 +0100

    added truncated

---
+++ js/data/43nt.js
@@ -0,0 +1,144 @@
+// Order n cubic
+
+import { rt2, rt3, p, p2, p_1 } from "./constants.js";
+import { boundaries } from "./geometry-decider.js";
+
+const cubeDataTrunc = (n) => {
+
+    const metric = boundaries(n, 4, 6);
+    const cos = Math.cos(2 * Math.PI / n);
+    const cot = 1 / (Math.tan(Math.PI / n) ** 2);
+
+    const d =
+        (n == 3) ? (v) => [
+            v[1],
+            v[0],
+            v[2],
+            v[3]
+        ] : (n == 4) ? (v) => [
+            v[0],
+            2 * v[0] - 1 * v[1],
+            v[2],
+            v[3]
+        ] : (n == 5) ? (v) => [
+            p * v[0] - p_1*v[1],
+            p2 * v[0] - p * v[1],
+            v[2],
+            v[3]
+        ] : (n == 6) ? (v) => [
+            2 * v[0] - v[1],
+            3 * v[0] - 2 * v[1],
+            v[2],
+            v[3]
+        ] : (v) => [
+            (1 + 2 * cos) * v[0] - 2 * cos * v[1],
+            2 * (1 + cos) * v[0] - (1 + 2 * cos) * v[1],
+            v[2],
+            v[3]
+        ];
+
+    // for n = 4: factor of 1/2 is arbitrary but scales shapes nicely
+    const f =
+        (n == 3) ? (v) => [
+            v[0] / 2,
+            v[1] / 2,
+            v[2] / 2,
+            v[3] / 2
+        ] : (n == 4) ? (v) => [
+            v[0],
+            v[1] / 2,
+            v[2] / 2,
+            v[3] / 2
+        ] : (n == 5) ? (v) => [
+            p2 * v[0] / rt2,
+            Math.sqrt(p / 2) * v[1],
+            Math.sqrt(p / 2) * v[2],
+            Math.sqrt(p / 2) * v[3]
+        ] : (n == 6) ? (v) => [
+            rt3 * v[0],
+            v[1],
+            v[2],
+            v[3]
+        ] : (v) => [
+            Math.sqrt(Math.abs(2 * cot / (3 - cot))) * v[0],
+            Math.sqrt(Math.abs((cot - 1) / (3 - cot))) * v[1],
+            Math.sqrt(Math.abs((cot - 1) / (3 - cot))) * v[2],
+            Math.sqrt(Math.abs((cot - 1) / (3 - cot))) * v[3]
+        ];
+
+    return {
+
+        vertices: [
+            [1, 1, 1, 1], [1, 1, -1, 1],
+            [1, -1, -1, 1], [1, -1, 1, 1],
+            [1, 1, 1, -1], [1, 1, -1, -1],
+            [1, -1, -1, -1], [1, -1, 1, -1]
+        ],
+
+        edges: [
+            [0, 3], [3, 2], [2, 1], [1, 0],
+            [7, 4], [4, 5], [5, 6], [6, 7],
+            [0, 4], [1, 5], [2, 6], [3, 7]
+        ],
+
+        faces: [
+            [0, 1, 2, 3], [4, 7, 3, 0], [7, 6, 2, 3],
+            [4, 5, 6, 7], [0, 1, 5, 4], [1, 2, 6, 5]
+        ],
+
+        numVertices: 8,
+
+        numEdges: 12,
+
+        numFaces: 6,
+
+        numSides: 4,
+
+        // CFE
+        // (0, 0, 0, 1)
+        a: (v) => [v[0], v[1], v[2], -v[3]],
+
+        // CFV
+        // (0, 0, 1, -1)
+        b: (v) => [v[0], v[1], v[3], v[2]],
+
+        // CEV
+        // (0, 1, -1, 0)
+        c: (v) => [v[0], v[2], v[1], v[3]],
+
+        // FEV
+        // (cot ** 2 - 1, 2 cot ** 2, 0, 0)
+        d: d,
+
+        // Identity matrix
+        e: (v) => v,
+
+        f: f,
+
+        faceReflections: ["bc", "c", "cbabc", "abc", "", "babc"],
+
+        outerReflection: "d",
+
+        // (1, 1, 1, 1)
+        V: [1, 1, 1, 1],
+
+        // (1, 1, 1, 0)
+        E: [1, 1, 1, 0],
+
+        // (1, 1, 0, 0)
+        F: [1, 1, 0, 0],
+
+        // (1, 0, 0, 0)
+        C: [1, 0, 0, 0],
+
+        // 3 4 5 6 7
+        // s e h d u
+        metric: metric,
+
+        cellType: "spherical"
+
+    }
+
+}
+
+export { cubeDataTrunc };
---
+++ js/main/main-edge-visible-truncated.js
@@ -0,0 +1,162 @@
+import * as THREE from "../three-bits/three.module.js";
+import { OrbitControls } from "../three-bits/orbit-controls.js";
+import { objectMaker } from "./object-maker.js";
+import * as EM from "./edge-maker.js";
+import { cubeDataTruncTrunc } from "../data/43nt.js";
+
+window.onload = main;
+
+function main() {
+
+    var p = 5, q = 3, r = 3;
+    var thetax = 0, thetay = 0, thetaz = 0, thetau = 0, thetav = 0, thetaw = 0;
+    var metric = "s";
+    var invisible = false;
+    var intersection = true;
+    var geom = {};
+
+    const canvas = document.getElementById("c");
+
+    var WIDTH = canvas.clientWidth;
+    var HEIGHT = canvas.clientHeight;
+
+    var renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setSize(WIDTH, HEIGHT);
+    canvas.appendChild(renderer.domElement);
+
+    const scene = new THREE.Scene();
+    scene.background = new THREE.Color(0xFFFFFF);
+
+    var camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 0.1, 100);
+    camera.position.set(0, 10, 0);
+    camera.up = new THREE.Vector3(0, 0, 1);
+
+    scene.add(camera);
+
+    var controls = new OrbitControls(camera, canvas);
+    controls.enabled = true;
+    controls.update();
+
+    var lineGroup = new THREE.Group();
+    scene.add(lineGroup);
+
+    var data = {
+        p: p,
+        q: q,
+        r: r,
+        model: "uhp",
+        refinement: 50,
+        intersection: intersection,
+        invisibleLines: invisible,
+        transform: "",
+        position: [0, 0, 0],
+        cells: [""],
+        numFaces: 200
+    }
+
+    geom = (rx, ry, rz, ru, rv, rw, camera) => EM.generateData(
+        cubeDataTrunc(5), rx, ry, rz, ru, rv, rw, data.refinement, data.intersection, data.invisibleLines, camera, data.cells
+    );
+
+    lineGroup.children = [geom(thetax, thetay, thetaz, thetau, thetav, thetaw, camera.position.toArray())];
+
+    // const geometry = new THREE.SphereBufferGeometry(2, 64, 64);
+    // const material1 = new THREE.MeshBasicMaterial({ color: 0xffff00, opacity: 0.5, transparent: true });
+    // const sphere1 = new THREE.Mesh(geometry, material1);
+    // sphere1.position.set(-1,-1,-1);
+    // scene.add(sphere1);
+
+    render();
+
+    window.addEventListener("resize", onWindowResize, false);
+
+    function onWindowResize() {
+
+        WIDTH = canvas.clientWidth;
+        HEIGHT = canvas.clientHeight;
+
+        renderer.setSize(WIDTH, HEIGHT);
+
+        camera.aspect = WIDTH / HEIGHT;
+        camera.updateProjectionMatrix();
+
+    }
+
+    function render() {
+
+        renderer.render(scene, camera);
+        requestAnimationFrame(render);
+
+    }
+
+    window.addEventListener('keydown', (event) => {
+        if (event.key === "Enter") {
+            geom = (rx, ry, rz, ru, rv, rw, camera) => EM.generateData(
+                cubeDataTrunc(5), rx, ry, rz, ru, rv, rw, data.refinement, data.intersection, data.invisibleLines, camera, data.cells
+            );
+            lineGroup.children = [geom(thetax, thetay, thetaz, thetau, thetav, thetaw, camera.position.toArray())];
+        }
+    });
+
+    window.addEventListener("touchend", () => {
+        geom = (rx, ry, rz, ru, rv, rw, camera) => EM.generateData(
+            cubeDataTrunc(5), rx, ry, rz, ru, rv, rw, data.refinement, data.intersection, data.invisibleLines, camera, data.cells
+        );
+        lineGroup.children = [geom(thetax, thetay, thetaz, thetau, thetav, theta, camera.position.toArray())];
+    }, false);
+
+    document.getElementById("myRangep").oninput = function () {
+        data.p = this.value;
+    };
+
+    document.getElementById("myRangeq").oninput = function () {
+        data.q = this.value;
+    };
+
+    document.getElementById("myRanger").oninput = function () {
+        data.r = this.value / 2;
+    };
+
+    document.getElementById("myRangex").oninput = function () {
+        thetax = Math.PI * this.value / 50;
+    };
+
+    document.getElementById("myRangey").oninput = function () {
+        thetay = Math.PI * this.value / 50;
+    };
+
+    document.getElementById("myRangez").oninput = function () {
+        thetaz = Math.PI * this.value / 50;
+    };
+
+    document.getElementById("myRangeu").oninput = function () {
+        (metric === "h" || metric === "p" || metric === "u") ? thetau = this.value / 30 :
+            (metric === "e") ? thetau = this.value / 10 :
+                (metric === "s") ? thetau = Math.PI * this.value / 50 : this.value;
+        lineGroup.children = [geom(thetax, thetay, thetaz, thetau, thetav, thetaw, camera.position.toArray())];
+    };
+
+    document.getElementById("myRangev").oninput = function () {
+        (metric === "h" || metric === "p" || metric === "u") ? thetav = this.value / 30 :
+            (metric === "e") ? thetav = this.value / 10 :
+                (metric === "s") ? thetav = Math.PI * this.value / 50 : this.value;
+        lineGroup.children = [geom(thetax, thetay, thetaz, thetau, thetav, thetaw, camera.position.toArray())];
+
+    };
+
+    document.getElementById("myRangew").oninput = function () {
+        (metric === "h" || metric === "p" || metric === "u") ? thetaw = this.value / 30 :
+            (metric === "e") ? thetaw = this.value / 10 :
+                (metric === "s") ? thetaw = Math.PI * this.value / 50 : this.value;
+        lineGroup.children = [geom(thetax, thetay, thetaz, thetau, thetav, thetaw, camera.position.toArray())];
+    };
+
+    document.getElementById("visibleLines").addEventListener("click", function () {
+        data.invisibleLines = !data.invisibleLines;
+    });
+
+    document.getElementById("intersection").addEventListener("click", function () {
+        data.intersection = !data.intersection;
+    });
+
+}
