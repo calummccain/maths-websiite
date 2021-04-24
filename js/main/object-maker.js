fe4541cc6436f1ebd891e6fa6bd426d71100fed8
Calum McCain <cmccain97@gmail.com>
2021-04-24 23:30:16 +0100

    35nt working!

--- js/main/object-maker.js
+++ js/main/object-maker.js
@@ -14,6 +14,7 @@ import { pqrData } from "../data/pqr.js";
 
 import { tetrahedronTruncData } from "../data/33nt.js";
 import { octahedronTruncData } from "../data/34nt.js";
+import { icosahedronTruncData } from "../data/35nt.js";
 import { cubeTruncData } from "../data/43nt.js";
 
 
@@ -33,6 +34,7 @@ function objectMaker(parameters) {
     const geomTrunc = {
         "{3,3}": tetrahedronTruncData,
         "{3,4}": octahedronTruncData,
+        "{3,5}": icosahedronTruncData,
         "{4,3}": cubeTruncData
     }
 
@@ -48,6 +50,7 @@ function objectMaker(parameters) {
     } else {
 
         data = geomTrunc["{" + p + "," + q + "}"](r);
+        console.log(data)
 
     }
 
