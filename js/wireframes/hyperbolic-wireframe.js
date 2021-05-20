8f43ea4b61990f0773ea649c04546258a43cdbdc
Calum McCain <cmccain97@gmail.com>
2021-05-20 11:54:30 +0100

    more work needed

--- js/wireframes/hyperbolic-wireframe.js
+++ js/wireframes/hyperbolic-wireframe.js
@@ -19,7 +19,7 @@ import { matrixDict } from "../maths-functions/generate-tesselations.js";
 const eps = 1e-5;
 
 function hyperbolicEdges(data, parameters) {
-
+    console.log(data)
     // Parameter checks
     const cells = parameters.cells || [""];
     const [thetax, thetay, thetaz, thetau, thetav, thetaw] = parameters.angles || [0, 0, 0, 0, 0, 0];
@@ -67,6 +67,8 @@ function hyperbolicEdges(data, parameters) {
         edges = edges.concat(generateEdges(localVertices));
 
     }
+
+    console.log(faces)
 
     outline();
 
@@ -139,7 +141,7 @@ function hyperbolicEdges(data, parameters) {
                 v3 = localVertices[data.faces[i][2]].hyperboloid;
 
             }
-
+            console.log(v1, v2, v3)
             if (model === "uhp") {
 
                 v1 = HF.hyperboloidToUpperHalfPlane(v1);
@@ -155,6 +157,8 @@ function hyperbolicEdges(data, parameters) {
                 centerModel = HF.hyperboloidToPoincare(centerHyperboloid);
 
             }
+
+            
 
             if (Math.abs(VF.determinant3([
                 VF.vectorDiff(v1, centerModel),
