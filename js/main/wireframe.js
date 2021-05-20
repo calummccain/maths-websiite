eadc02bf7c995b8404808cf1e3afed22fd4d751e
Calum McCain <cmccain97@gmail.com>
2021-05-20 11:54:14 +0100

    debugging

--- js/main/wireframe.js
+++ js/main/wireframe.js
@@ -8,7 +8,7 @@ window.onload = main;
 
 function main() {
 
-    var p = 4, q = 3, r = 6;
+    var p = 3, q = 4, r = 4;
     var thetax = 0, thetay = 0, thetaz = 0, thetau = 0, thetav = 0, thetaw = 0;
     var invisible = false;
     var geom = {};
