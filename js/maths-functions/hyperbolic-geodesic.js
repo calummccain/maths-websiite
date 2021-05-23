5001864260e91b472aa02d84e11783cfff782707
Calum McCain <cmccain97@gmail.com>
2021-05-23 14:46:49 +0100

    improved functionality

--- js/maths-functions/hyperbolic-geodesic.js
+++ js/maths-functions/hyperbolic-geodesic.js
@@ -2,20 +2,24 @@
 // Given two endpoints, generate the hyperbolic geodesic
 // between them
 // 
-// Inputs: a, b, number, model
+// Inputs: a, b, number
 // Output: array of intermediary points
 //
 // Change history:
 //     18/05/21 Initial commit
 //     23/05/21 Added VF and updates inputs
+//              Returns both poincare and upper half plane
+//              geodesics as a dictionary
 //=========================================================
 
 import * as HF from "./hyperbolic-functions.js";
 import * as VF from "./vector-functions.js";
 
-function hyperbolicGeodesic(p1, p2, number, model) {
+function hyperbolicGeodesic(p1, p2, number) {
 
-    var edge = [];
+    var edgePoincare = [];
+    var edgeUpperHalfPlane = [];
+    var p;
     var ratios = [];
 
     const denom = 1 / Math.sqrt(Math.abs(2 * HF.hyperboloidInnerProduct(p1.hyperboloid, p2.hyperboloid)));
@@ -29,25 +33,16 @@ function hyperbolicGeodesic(p1, p2, number, model) {
 
     }
 
-    if (model === "uhp") {
+    for (var i = 0; i <= number; i++) {
 
-        for (var i = 0; i <= number; i++) {
+        p = VF.vectorSum([VF.vectorScale(p1.hyperboloid, ratios[i]), VF.vectorScale(p2.hyperboloid, ratios[number - i])]);
 
-            edge.push(HF.hyperboloidToUpperHalfPlane(VF.vectorSum([VF.vectorScale(p1.hyperboloid, ratios[i]), VF.vectorScale(p2.hyperboloid, ratios[number - i])])));
-
-        }
-
-    } else if (model === "poincare") {
-
-        for (var i = 0; i <= number; i++) {
-
-            edge.push(HF.hyperboloidToPoincare(VF.vectorSum([VF.vectorScale(p1.hyperboloid, ratios[i]), VF.vectorScale(p2.hyperboloid, ratios[number - i])])));
-
-        }
+        edgePoincare.push(HF.hyperboloidToPoincare(p));
+        edgeUpperHalfPlane.push(HF.hyperboloidToUpperHalfPlane(p));
 
     }
 
-    return edge;
+    return { poincare: edgePoincare, uhp: edgeUpperHalfPlane };
 
 }
 
