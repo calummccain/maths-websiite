40ee9c22b1215b88c97fcb7c38cd3c830a07438a
Calum McCain <cmccain97@gmail.com>
2021-06-30 21:11:57 +0100

    reduced word initial commit

---
+++ js/maths-functions/reduce-word.js
@@ -0,0 +1,61 @@
+function reduceWord(word, p, q, r) {
+
+    var done = false;
+    var w1 = word;
+    var w2 = word;
+
+    var reduced = [
+        ["aa", ""],
+        ["bb", ""],
+        ["cc", ""],
+        ["dd", ""],
+        ["aca", "c"],
+        ["cac", "a"],
+        ["ada", "d"],
+        ["dad", "a"],
+        ["bdb", "d"],
+        ["dbd", "b"],
+        ["ab".repeat(Math.ceil(p / 2)) + ((p % 2 == 1) ? "" : "a"), "ba".repeat(Math.ceil(p / 2) - 1) + ((p % 2 == 1) ? "" : "b")],
+        ["ba".repeat(Math.ceil(p / 2)) + ((p % 2 == 1) ? "" : "b"), "ab".repeat(Math.ceil(p / 2) - 1) + ((p % 2 == 1) ? "" : "a")],
+        ["bc".repeat(Math.ceil(q / 2)) + ((q % 2 == 1) ? "" : "b"), "cb".repeat(Math.ceil(q / 2) - 1) + ((q % 2 == 1) ? "" : "c")],
+        ["cb".repeat(Math.ceil(q / 2)) + ((q % 2 == 1) ? "" : "c"), "bc".repeat(Math.ceil(q / 2) - 1) + ((q % 2 == 1) ? "" : "b")],
+        ["cd".repeat(Math.ceil(r / 2)) + ((r % 2 == 1) ? "" : "c"), "dc".repeat(Math.ceil(r / 2) - 1) + ((r % 2 == 1) ? "" : "d")],
+        ["dc".repeat(Math.ceil(r / 2)) + ((r % 2 == 1) ? "" : "d"), "cd".repeat(Math.ceil(r / 2) - 1) + ((r % 2 == 1) ? "" : "c")]
+    ]
+
+    while (!done) {
+
+        w2 = partialReduce(w1, reduced);
+
+        if (w1 === w2) {
+
+            done = true;
+
+        }
+
+        w1 = w2;
+        continue;
+
+    }
+
+    return w1;
+
+}
+
+function partialReduce(newWord, reduced) {
+
+    for (var i = 0; i < 16; i++) {
+
+        if (newWord.includes(reduced[i][0])) {
+
+            newWord = newWord.replace(reduced[i][0], reduced[i][1]);
+
+        }
+
+    }
+
+    return newWord;
+
+}
+
+export { reduceWord };
